4e22bb511b033757e2fddafceffee42a5dd99f9b
Joe Wang <joewang705@gmail.com>
2022-06-21 17:41:29 -0700

    nthfibonacci and palindrome

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(string) {
+    // Write your code here.
+    let i = 0;
+    let j = string.length - 1;
+  
+    while (i<j){
+      if (string[i] !== string[j]) return false;
+      i++;
+      j--;
+    }
+  
+    return true;
+  }
+  
+  // Do not edit the line below.
+  exports.isPalindrome = isPalindrome;
+  
